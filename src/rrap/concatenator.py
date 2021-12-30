4e7600374d95d047f183b8fff446d48d25911d41
Conner Kojima <connerkojima@Conners-MacBook-Pro.local>
2021-12-29 21:26:44 -1000

    adding more options

--- src/rrap/concatenator.py
+++ src/rrap/concatenator.py
@@ -8,28 +8,27 @@ class Concatenator:
         self.cat_file_name = os.path.join(self.args.o, 'allgenomes_cat_{0}.fna'.format(self.args.n))
 
     def concatenate(self):
-        if os.path.exists(self.args.rg):
-            # replace contigs with file name
+        # replace contigs with file name
 
-            # delete cat file if it already exists
-            print("checking if allgenome_cat file exists:", self.cat_file_name)
-            if os.path.exists(self.cat_file_name):
-                print("deleting cat file")
-                subprocess.run('rm -f {0}'.format(self.cat_file_name), shell=True)
+        # delete cat file if it already exists
+        print("checking if allgenome_cat file exists:", self.cat_file_name)
+        if os.path.exists(self.cat_file_name):
+            print("deleting and recreating cat file")
+            subprocess.run('rm -f {0}'.format(self.cat_file_name), shell=True)
 
+        if self.args.contig_merge:
             # get list of all ref genome file paths
             cmd = subprocess.run('ls {0}'.format(os.path.join(self.args.rg, "*")),
-                                 shell=True, capture_output=True, text=True)
+                                    shell=True, capture_output=True, text=True)
             rf_list = cmd.stdout.split()
 
             # read into each rf genome and change headers to group contigs under organism header (file name)
             for rf_path in rf_list:
                 self.combine_contigs(rf_path)
-
-            return self.cat_file_name
-
         else:
-            raise Exception("reference genomes input dir not specified (OPTION -rg)")
+            # concatenate all ref genomes into one file
+            subprocess.run("cat {0} > {1}".format(os.path.join(self.args.rg, "*.fa"), self.cat_file_name))
+        return self.cat_file_name
 
     def combine_contigs(self, rf_path):
         # replace initial header line with line that contains just organism e.g. (>Candidatus Fonsibacter)
