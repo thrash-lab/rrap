f38e867073eea4eee334b48d1b80fd4b560d6def
Conner Kojima <connerkojima@Conners-MacBook-Pro.local>
2022-05-31 11:19:56 -0700

    restucturing dir

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='rrap',
-      version='1.1.1',
+      version='1.1.2',
       description='A metagenomic read recruitment data pipeline',
       url='https://github.com/Kojiconner/rrap_metag_pkg/',
       author='Conner Kojima',
